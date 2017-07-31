70bc7c13b1e498f13e35fa6315112922d70a8c62
Humbertzhang <504490160@qq.com>
2017-07-31 23:45:53 +0800

    add smothing to shell and intro2ed.md

--- tinygoogle.sh
+++ tinygoogle.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+FLAG=0
+while ((${FLAG} == 0))
+do 
+    read -p "Input port(between 3001 and 9999):" PORT
+    if ((PORT >= 3001 )) && ((PORT <=9999));
+        then
+            FLAG=1
+        else
+            echo "Error:port should between 3001 and 9999"
+    fi
+done
 
-gunicorn --name tinygoogle -b 0.0.0.0:5488 -w 2 app:app
+echo
+echo "You Project Start!"
+echo
 
+gunicorn --name tinygoogle -b 0.0.0.0:${PORT} -w 2 app:app
